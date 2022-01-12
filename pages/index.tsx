913ab4884f50fcf66d863e5ae01577ce95f1ee4d
Oussama El Zein <oussama.elzein@gmail.com>
2022-01-12 13:35:14 -0800

    Nextjs Typescript and Styled Components

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+function HomePage() {
+
+    // Create a Title component that'll render an <h1> tag with some styles
+  const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+  `;
+
+  // Create a Wrapper component that'll render a <section> tag with some styles
+  const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+  `;
+
+  return (
+    <Wrapper>
+      <Title>
+        Hello World!
+      </Title>
+    </Wrapper>
+  )
+}
+
+export default HomePage
