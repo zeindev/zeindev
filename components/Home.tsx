ccbbc159da91f5f989677df46c44d6ba62fdef8b
Oussama El Zein <oussama.elzein@gmail.com>
2022-01-15 13:42:12 -0800

    Theme colour tweaks

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,10 +1,12 @@
-import styles from '../styles/Home.module.scss';
+import styles from "../styles/Home.module.scss";
+import cn from "classnames";
+import Link from "next/link";
 
 function Home() {
   return (
     <section className={styles.home}>
       <h1 className={styles.name}>Oussama El Zein</h1>
-      <div className={styles['item-container']}>
+      <div className={styles["item-container"]}>
         <h3 className={styles.item}>Welcome</h3>
         <img className={styles.image} src="/images/oussama.jpg" />
       </div>
