f26a0ac05512cdf20ac92ed74a199174656bcaf4
Oussama El Zein <oussama.elzein@gmail.com>
2022-01-15 17:58:03 -0800

    print media queries and more font weight tweaks

--- components/ResumeHeader.tsx
+++ components/ResumeHeader.tsx
@@ -5,7 +5,7 @@ export default function ResumeHeader({ resume }) {
     <section className={styles['resume-header']}>
       <h1 className={styles.name}>{resume.name}</h1>
       <section className={styles['section-container']}>
-        <h3 className={styles.item}>{resume.position}</h3>
+        <h3 className={styles.subtitle}>{resume.position}</h3>
         <section className={styles['item-container']}>
           <h3 className={styles.item}>{resume.phone}</h3>
           <h3 className={styles.item}>{`${resume.city} ${resume.province}`}</h3>
